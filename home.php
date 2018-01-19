351ac3da05108fe93dc7f2dbc5a361ef3bcb31fc
Myles Enriquez <enri.myles@gmail.com>
2018-01-19 15:52:36 -0800

    Test commit on branch.

--- home.php
+++ home.php
@@ -43,7 +43,7 @@ get_header(); ?>
                 <div class="row">
 
                   <div class="col-sm-8">
-                    Changed page title again.
+                    Changed page title on new branch.
                     <?php if ( have_posts() ) : ?>
                       <?php
                       // Start the loop.
