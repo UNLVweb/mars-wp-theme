bf46f5ff7f2fd1e03dc0c934ef61de84594c9f9e
Phillip Busche <phillip.busche@unlv.edu>
2018-01-22 13:49:59 -0800

    front page development

--- front-page.php
+++ front-page.php
@@ -43,7 +43,7 @@ get_header(); ?>
                 <div class="row">
 
                   <div class="col-sm-8">
-                    Front Page
+                    Front Page 1
                     <?php if ( have_posts() ) : ?>     
                       <?php
                       // Start the loop.
